733b565175edd4328d3d4784a601955cb4990be6
FercanSen <fercansen@gmail.com>
2024-08-27 21:42:44 +0300

    Fix folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ProductListPage from "./components/pages/ProductListPage";
+import ProductListPage from "./pages/ProductListPage";
 // import ProductDetailPage from "./components/pages/ProductDetailPage";
 
 function App() {
